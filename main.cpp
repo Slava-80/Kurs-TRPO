e909ba514bab84bc3607dd8c71696c52e5d265a5
Slava-80 <slava.bykanov@mail.ur>
2020-06-24 02:56:52 +0700

    Replaced fuinction genering_word in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,50 +6,35 @@
 #include <stdlib.h>
 
 void genering_word() {
-    char ch, sl_one[100], sl_two[100], sl_three[100];
-	int k = 1, p = 0, word = 0, r = 0, s = 0;
-	srand(time(NULL));
-	FILE *tf;
-	tf = fopen("Infinitive.txt", "r");
-	int num_word = rand()%118;
-	  while((ch = fgetc(tf)) != EOF) {
-	    if(ch == ' ')
-	       word++;
-	    if(k == num_word) {
-	      if(word == 0) {
-	        sl_one[p] = ch;
-	        p++;
-	     }
-	      if(word == 1) {
-	        sl_two[r] = ch;
-	        r++;
-	     }
-	      if(word == 2) {
-	        sl_three[s] = ch;
-	        s++;
-	      }
-	}
-	    if(ch == '\n') {
-	      k++;
-	    }
-	}
-	fclose(tf);
+int word = 0;
+char ch;
+int k = 1;
+srand(time(NULL));
+FILE *tf, *pr;
+tf = fopen("Infinitive.txt", "r");
+pr = fopen("werbs.txt", "w");
+int num_word = rand()%118;
+while((ch = fgetc(tf)) != EOF) {
+if(ch == ' ') {
+word++;
+}
+if(k == num_word) {
+if(word == 0) {
+fputc(ch, pr);
+}
+if(word == 1) {
+fputc(ch, pr);
+}
+if(word == 2) {
+fputc(ch, pr);
+}
+}
+if(ch == '\n') {
+k++;
+word = 0;
+}
+}
+fclose(tf);
+fclose(pr);
 }
 
-void output_word(int word) {
-	if(word == 0) {
-	   cout « "Infinitive: ";
-	     for(int i = 0; i < p; i++)
-	       sl_one[i];
-	}
-	if(word == 1) {
-	   cout « "Past Simple: ";
-	     for(int i = 0; i < r; i++)
-	       sl_two[i];
-	}
-	if(word == 2) {
-	   cout « "PastParticiple: ";
-	     for(int i = 0; i < s; i++)
-	       sl_three[i];
-	}
-}
